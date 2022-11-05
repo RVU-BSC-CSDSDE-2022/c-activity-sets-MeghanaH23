59936ef636e7f8297dd34ddbfb5d0ab7264275cb
Meghana  H <112924802+MeghanaH23@users.noreply.github.com>
2022-11-05 13:24:20 +0000

    completed all the problems in set 01

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,9 +1,25 @@
-#include <stdio.h>
+7include <stdio.h>
 int import();
 int compare (int a,int b,int c);
 void output (int a,int b,int c, int largest);
 
 int main(){
   int a,b,c,l;
-  a=input
+  a=input();
+  b=input();
+  c=input();
+  b=input();
+  output(a,b,c,l);
+}
+
+int input () {
+  int x;
+  printf("Enter thr number\n");
+  scanf("%d,&x");
+  return(x);
+  }
+
+int compare(int a,int b,int c){
+  if(a>b)
+    
 }
